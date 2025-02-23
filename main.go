1dc062592dd8aa98fae559c6dfacd01fb3859e9b
Diego Pereira <diego.pereira@fortytwo.io>
2025-02-23 17:57:07 +0000

    feat: Add net/http packages and create the first endpoint

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"fmt"
 	"go-helloworld-api/hello"
+	"net/http"
 )
 
-func main() {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	message := hello.Hello("Diego")
-	fmt.Println(message)
+	fmt.Fprintf(w, message)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Starting server on :8080..")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
